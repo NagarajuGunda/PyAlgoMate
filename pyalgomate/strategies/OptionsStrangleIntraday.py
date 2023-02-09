a9e08e41f6d037f91629a7462bc46fac405fec63
Nagaraju Gunda <gunda.nagaraju92@gmail.com>
2023-02-09 21:02:02 +0530

    Using fixed length deque to handle websocket data non in same seconds

--- pyalgomate/strategies/OptionsStrangleIntraday.py
+++ pyalgomate/strategies/OptionsStrangleIntraday.py
@@ -1,4 +1,6 @@
 import logging
+from collections import deque
+
 from pyalgotrade import strategy
 from pyalgotrade import bar
 
@@ -56,6 +58,8 @@ class OptionsStrangleIntraday(strategy.BaseStrategy):
         super(OptionsStrangleIntraday, self).__init__(feed, broker)
         self.__reset__()
         self.currentDate = None
+        self.bars = {}
+        self.maxLen = 255
         if resampleFrequency:
             self.resampleBarFeed(resampleFrequency, self.resampledOnBars)
 
@@ -63,11 +67,23 @@ class OptionsStrangleIntraday(strategy.BaseStrategy):
         logger.info("Resampled {0} {1}".format(bars.getDateTime(),
                                                bars[self.underlyingInstrument].getClose()))
 
+    def pushBars(self, bars):
+        for key, value in bars.items():
+            if key not in self.bars:
+                self.bars[key] = deque(maxlen=self.maxLen)
+            self.bars[key].append(value)
+
     def onBars(self, bars):
-        bar = bars.getBar(self.underlyingInstrument)
+        # push the incoming bars into bars dict
+        # bars contains a queue of maxLen number of bar data for each instrument
+        # access last index element of the queue for the latest bar data
+        self.pushBars(bars)
+
+        bar = self.bars.get(self.underlyingInstrument, None)
         if not bar:
             return
 
+        bar = bar[-1]    # get the latest bar
         dateTime = bar.getDateTime()
 
         if dateTime.date() != self.currentDate:
@@ -84,11 +100,11 @@ class OptionsStrangleIntraday(strategy.BaseStrategy):
             self.ceSymbol, self.peSymbol = findOptionSymbols(
                 self.underlyingInstrument, ceStrike, peStrike)
 
-        if (not bars.getBar(self.ceSymbol)) or (not bars.getBar(self.peSymbol)):
+        if (not self.bars.get(self.ceSymbol, None)) or (not self.bars.get(self.peSymbol, None)):
             return
 
-        ceLTP = bars[self.ceSymbol].getClose()
-        peLTP = bars[self.peSymbol].getClose()
+        ceLTP = self.bars[self.ceSymbol][-1].getClose()
+        peLTP = self.bars[self.peSymbol][-1].getClose()
 
         if self.cePosition and ((ceLTP > self.ceSL) or (ceLTP < self.ceTarget)):
             self.cePosition = self.cePosition.exitMarket()
