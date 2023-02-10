5159c0110dd69fb69baa3a9f04fb5a1c6abcdd81
Nagaraju Gunda <gunda.nagaraju92@gmail.com>
2023-02-11 00:11:32 +0530

    Getting the ltp and option symbols to subscribe for websocket

--- trade.py
+++ trade.py
@@ -1,35 +1,41 @@
 import yaml
 import pyotp
 import logging
+import datetime
 
 from pyalgomate.brokers.finvasia.feed import LiveTradeFeed
 from pyalgomate.brokers.finvasia.broker import PaperTradingBroker
+import pyalgomate.brokers.finvasia as finvasia
 from pyalgomate.strategies.OptionsStrangleIntraday import OptionsStrangleIntraday
+import pyalgomate.utils as utils
 
 from NorenRestApiPy.NorenApi import NorenApi as ShoonyaApi
 
 logging.basicConfig(level=logging.DEBUG)
 
 
+def getToken(api, exchangeSymbol):
+    splitStrings = exchangeSymbol.split('|')
+    exchange = splitStrings[0]
+    symbol = splitStrings[1]
+    ret = api.searchscrip(exchange=exchange, searchtext=symbol)
+
+    if ret != None:
+        for value in ret['values']:
+            if value['instname'] == 'OPTIDX' and value['tsym'] == symbol:
+                return value['token']
+            if value['instname'] == 'UNDIND' and value['cname'] == symbol:
+                return value['token']
+
+    return None
+
+
 def getTokenMappings(api, exchangeSymbols):
     tokenMappings = {}
 
     for exchangeSymbol in exchangeSymbols:
-        splitStrings = exchangeSymbol.split('|')
-        exchange = splitStrings[0]
-        symbol = splitStrings[1]
-        ret = api.searchscrip(exchange=exchange, searchtext=symbol)
-
-        if ret != None:
-            for value in ret['values']:
-                if value['instname'] == 'OPTIDX' and value['tsym'] == symbol:
-                    tokenMappings["{0}|{1}".format(
-                        value['exch'], value['token'])] = exchangeSymbol
-                    break
-                if value['instname'] == 'UNDIND' and value['cname'] == symbol:
-                    tokenMappings["{0}|{1}".format(
-                        value['exch'], value['token'])] = exchangeSymbol
-                    break
+        tokenMappings["{0}|{1}".format(exchangeSymbol.split(
+            '|')[0], getToken(api, exchangeSymbol))] = exchangeSymbol
 
     return tokenMappings
 
@@ -45,10 +51,18 @@ def main():
                     vendor_code=cred['vc'], api_secret=cred['apikey'], imei=cred['imei'])
 
     if ret != None:
+        underlyingInstrument = 'NSE|NIFTY BANK'
+
+        ltp = api.get_quotes('NSE', getToken(api, underlyingInstrument))['lp']
+
+        optionSymbols = finvasia.broker.getOptionSymbols(underlyingInstrument, utils.getNearestWeeklyExpiryDate(
+            datetime.datetime.now().date()), ltp, 5)
+
         barFeed = LiveTradeFeed(api, getTokenMappings(
-            api, ["NSE|NIFTY INDEX", "NSE|NIFTY BANK", "NFO|BANKNIFTY09FEB23C41900", "NFO|BANKNIFTY09FEB23P41300"]))
+            api, ["NSE|NIFTY INDEX", underlyingInstrument] + optionSymbols))
         broker = PaperTradingBroker(200000, barFeed)
-        strat = OptionsStrangleIntraday(barFeed, broker, 'NSE|NIFTY BANK')
+
+        strat = OptionsStrangleIntraday(barFeed, broker, underlyingInstrument)
 
     strat.run()
 
