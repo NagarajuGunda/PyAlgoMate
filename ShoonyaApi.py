ac26be97e7d4b443d4fd8908e679766a42428f8c
Nagaraju Gunda <gunda.nagaraju92@gmail.com>
2023-04-02 20:30:27 +0530

    ShoonyAPI order test changes

--- ShoonyaApi.py
+++ ShoonyaApi.py
@@ -138,10 +138,14 @@ if ret != None:
         prompt1=input('what shall we do? ').lower()        
             
         if prompt1 == 'p':
-            ret = api.place_order(buy_or_sell='B', product_type='C',
-                        exchange='NSE', tradingsymbol='INFY-EQ', 
-                        quantity=1, discloseqty=0,price_type='LMT', price=1500.00, trigger_price=None,
-                        retention='DAY', remarks='my_order_001')
+            # ret = api.place_order(buy_or_sell='B', product_type='C',
+            #             exchange='NSE', tradingsymbol='INFY-EQ', 
+            #             quantity=1, discloseqty=0,price_type='LMT', price=1500.00, trigger_price=None,
+            #             retention='DAY', remarks='my_order_001')
+            ret = api.place_order(buy_or_sell='B', product_type='I',
+                        exchange='NSE', tradingsymbol='YESBANK-EQ', 
+                        quantity=1, discloseqty=0,price_type='MKT', price=None, trigger_price=None,
+                        retention='DAY', remarks='test_order')
             print(ret)
 
         elif prompt1 == 'm':
