13b10486e45f1c0a2c46adca6d840f9440941564
Nagaraju Gunda <gunda.nagaraju92@gmail.com>
2023-09-29 12:28:43 +0530

    Do not build open orders on startup

--- pyalgomate/strategies/BaseOptionsGreeksStrategy.py
+++ pyalgomate/strategies/BaseOptionsGreeksStrategy.py
@@ -300,6 +300,7 @@ class BaseOptionsGreeksStrategy(strategy.BaseStrategy):
     def onStart(self):
         # build open orders from tradeDf
         #self.buildOrdersFromActiveOrders()
+        pass
 
     def onEnterOk(self, position: position):
         execInfo = position.getEntryOrder().getExecutionInfo()
