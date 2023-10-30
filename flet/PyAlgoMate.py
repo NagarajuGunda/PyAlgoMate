3213504764582ad0765e83177fc420b285b8a298
Nagaraju Gunda <gunda.nagaraju92@gmail.com>
2023-10-31 00:20:42 +0530

    Flet UI Updates

--- flet/PyAlgoMate.py
+++ flet/PyAlgoMate.py
@@ -31,74 +31,191 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 
 
 class StrategyCard(ft.Card):
-    def __init__(self, strategy: BaseOptionsGreeksStrategy):
+    def __init__(self, strategy: BaseOptionsGreeksStrategy, page: ft.Page):
         super().__init__()
 
         self.strategy = strategy
+        self.page = page
         self.expand = True
         self.stateText = ft.Text(
-            self.strategy.state
+            self.strategy.state,
+            size=20
         )
         self.pnlText = ft.Text(
             "₹ 0",
-            size=30
+            size=25
+        )
+
+        self.openPositions = ft.Text(
+            'Open Pos: 0',
+            size=12
+        )
+        self.closedPositions = ft.Text(
+            'Closed Pos: 0',
+            size=12
+        )
+
+        self.closeDialogModel = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Please Confirm"),
+            content=ft.Text("Do you really want to square off all positions?"),
+            actions=[
+                ft.TextButton("Yes", on_click=self.squareOff),
+                ft.TextButton("No", on_click=self.closeDialog),
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+            on_dismiss=lambda e: print("Modal dialog dismissed!"),
         )
 
         self.content = ft.Row(
             height=100,
             controls=[
                 ft.Container(
+                    ft.Column(
+                        [
+                            ft.Text(
+                                strategy.strategyName,
+                                size=15,
+                                weight='w700'
+                            ),
+                            ft.Row(
+                                [
+                                    self.openPositions,
+                                    self.closedPositions
+                                ]
+                            )
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER
+                    ),
                     expand=1,
-                    content=ft.Text(
-                        strategy.strategyName
-                    )
+                    padding=ft.padding.only(left=50)
                 ),
                 ft.Container(
-                    expand=1,
-                    content=self.stateText
+                    ft.Column(
+                        [self.stateText],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                    ),
+                    expand=1
                 ),
                 ft.Container(
-                    expand=1,
-                    content=self.pnlText
+                    ft.Column(
+                        [self.pnlText],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                    ),
+                    expand=1
                 ),
                 ft.Container(
-                    expand=1,
-                    content=ft.Container(
-                        content=ft.ElevatedButton(
-                            text='Trades'
-                        )
-                    )
+                    ft.Column([ft.ElevatedButton(
+                        text='Trades',
+                        color='white',
+                        bgcolor='#263F6A'
+                    )],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        expand=1
+                    ),
+                    expand=1
+                ),
+                ft.Container(
+                    ft.Column([
+                        ft.TextButton("Square Off", icon="close_rounded", icon_color="red400",
+                                      on_click=self.openDialog)
+                    ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        expand=1
+                    ),
+                    expand=1
                 )
             ]
         )
 
-        self.strategy.getFeed().getNewValuesEvent().subscribe(self.onBars)
-
-    def onBars(self, dateTime, bars):
+    def onBars(self):
         self.stateText.value = str(self.strategy.state)
         pnl = self.strategy.getOverallPnL()
         self.pnlText.value = f'₹ {pnl:.2f}'
         self.pnlText.color = "green" if pnl >= 0 else "red"
+        self.openPositions.value = f'Open Pos: {len(self.strategy.openPositions)}'
+        self.closedPositions.value = f'Closed Pos: {len(self.strategy.closedPositions)}'
         self.update()
+
+    def squareOff(self, e):
+        self.closeDialogModel.open = False
+        self.strategy.closeAllPositions()
+        self.page.snack_bar = ft.SnackBar(
+            ft.Row([ft.Text(f"Closing all positions !!!", size=20)],
+                   alignment='center'),
+            bgcolor='#263F6A'
+        )
+        self.page.snack_bar.open = True
+        self.page.update()
+
+    def closeDialog(self, e):
+        self.closeDialogModel.open = False
+        self.page.update()
+
+    def openDialog(self, e):
+        self.page.dialog = self.closeDialogModel
+        self.closeDialogModel.open = True
+        self.page.update()
 
 
 class StrategiesContainer(ft.Container):
-    def __init__(self, strategies: List[BaseOptionsGreeksStrategy]):
+    def __init__(self, page: ft.Page, feed, strategies: List[BaseOptionsGreeksStrategy]):
         super().__init__()
-
+        self.padding = ft.padding.only(top=20)
         self.strategies = strategies
-        self.content = ft.Column(
-            horizontal_alignment='center',
-            controls=[
-                ft.Row(
-                    controls=[
-                        StrategyCard(strategy)]
-                ) for strategy in strategies
-            ]
+        self.page = page
+
+        feed.getNewValuesEvent().subscribe(self.onBars)
+
+        self.totalMtm = ft.Text(
+            '₹ 0', size=25)
+
+        totalMtmRow = ft.Container(
+            ft.Column([
+                ft.Container(ft.Text('Total MTM', size=15, weight=ft.FontWeight.BOLD,
+                             color='white'), padding=ft.padding.only(top=10, left=10)),
+                ft.Container(self.totalMtm, padding=ft.padding.only(left=10)),
+                ft.Container(
+                    ft.Column([
+                        ft.Divider(color='white'),
+                        ft.Text('MTM Graph  -->', size=15,
+                                weight=ft.FontWeight.W_400, color='white')
+                    ]),
+                    padding=ft.padding.only(
+                        top=35, left=10, right=10, bottom=10)
+                )
+            ]),
+            width=200,
+            height=200,
+            bgcolor='#263F6A',
+            border_radius=10
         )
 
+        self.strategyCards = [StrategyCard(
+            strategy, page) for strategy in self.strategies]
+        rows = [totalMtmRow]
+        rows.extend([ft.Row([strategyCard])
+                    for strategyCard in self.strategyCards])
+        self.content = ft.Column(
+            rows
+        )
 
-def GetStrategies():
+    def onBars(self, dateTime, bars):
+        for strategyCard in self.strategyCards:
+            strategyCard.onBars()
+
+        totalMtm = sum([strategy.getOverallPnL()
+                       for strategy in self.strategies])
+        self.totalMtm.value = f'₹ {totalMtm:.2f}'
+        self.totalMtm.color = "green" if totalMtm >= 0 else "red"
+        self.update()
+
+
+def GetFeedNStrategies():
     with open("strategies.yaml", "r") as file:
         config = yaml.safe_load(file)
 
@@ -148,7 +265,7 @@ def GetStrategies():
                 f'Error in creating strategy instance for <{strategyName}>. Error: {e}')
             logger.exception(traceback.format_exc())
 
-    return strategies
+    return feed, strategies
 
 
 def runStrategy(strategy):
@@ -174,10 +291,10 @@ def threadTarget(strategy):
 def main(page: ft.Page):
     page.horizontal_alignment = "center"
     page.vertical_alignment = "center"
-    page.padding = ft.padding.only(right=50)
+    page.padding = ft.padding.only(left=50, right=50)
     page.bgcolor = "#212328"
 
-    strategies = GetStrategies()
+    feed, strategies = GetFeedNStrategies()
     t = ft.Tabs(
         selected_index=0,
         animation_duration=300,
@@ -186,7 +303,8 @@ def main(page: ft.Page):
         tabs=[
             ft.Tab(
                 text="Strategies",
-                content=StrategiesContainer(strategies),
+                content=StrategiesContainer(
+                    page=page, feed=feed, strategies=strategies),
             ),
             ft.Tab(
                 text="Trade Terminal",
@@ -211,4 +329,4 @@ def main(page: ft.Page):
 
 
 if __name__ == "__main__":
-    ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8080)
