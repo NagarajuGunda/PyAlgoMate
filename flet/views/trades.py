bf4080782d5619f828cf4da19ae8bd6a7c8b5142
Nagaraju Gunda <gunda.nagaraju92@gmail.com>
2024-03-02 14:17:09 +0530

    Datatable changes

--- flet/views/trades.py
+++ flet/views/trades.py
@@ -15,43 +15,32 @@ class TradesView(ft.View):
             )
 
         columns=[
-            ft.DataColumn(ft.Text("S.NO.")),
-            ft.DataColumn(ft.Text("Symbol")),
-            ft.DataColumn(ft.Text("Entry Price")),
-            ft.DataColumn(ft.Text("Entry Time")),
-            ft.DataColumn(ft.Text("Exit Price")),
+            ft.DataColumn(ft.Text("Instrument")),
+            ft.DataColumn(ft.Text("Entry Price"), numeric=True),
+            ft.DataColumn(ft.Text("Entry Time"), on_sort=lambda e: print(f"{e.column_index}, {e.ascending}")),
+            ft.DataColumn(ft.Text("Exit Price"), numeric=True),
             ft.DataColumn(ft.Text("Exit Time")),
-            ft.DataColumn(ft.Text("PNL")),
-            ft.DataColumn(ft.Text("Action"))
+            ft.DataColumn(ft.Text("PNL"), numeric=True),
+            ft.DataColumn(ft.Text("Exit"))
         ]
         rows = []
-        index = 1
         for position in self.strategy.getActivePositions().copy():
-            rows.append(ft.DataRow(cells=[
-                        ft.DataCell(ft.Text(index)),
+            rows.append(
+                ft.DataRow(
+                    [
                         ft.DataCell(ft.Text(position.getInstrument())),
                         ft.DataCell(ft.Text(position.getEntryOrder().getAvgFillPrice() if position.getEntryOrder() else '')),
                         ft.DataCell(ft.Text(position.getEntryOrder().getSubmitDateTime() if position.getEntryOrder() else '')),
                         ft.DataCell(ft.Text(position.getExitOrder().getAvgFillPrice() if position.getExitOrder() else '')),
                         ft.DataCell(ft.Text(position.getExitOrder().getSubmitDateTime() if position.getExitOrder() else '')),
                         ft.DataCell(ft.Text(position.getPnL())),
-                        ft.DataCell(ft.TextButton(text="Exit Position")),
-                    ]))
-            index = index + 1
+                        ft.DataCell(ft.TextButton(icon="close_rounded", icon_color="red400")),
+                    ]
+                )
+            )
         datatable = ft.DataTable(
-            bgcolor="white",
-            border=ft.border.all(2, "red"),
-            border_radius=10,
-            vertical_lines=ft.border.BorderSide(3, "red"),
-            horizontal_lines=ft.border.BorderSide(1, "red"),
-            sort_column_index=0,
+            sort_column_index=2,
             sort_ascending=True,
-            heading_row_color=ft.colors.BLACK12,
-            heading_row_height=100,
-            data_row_color={"hovered": "0x30FF0000"},
-            show_checkbox_column=True,
-            divider_thickness=0,
-            column_spacing=100,
             columns=columns,
             rows=rows)
 
