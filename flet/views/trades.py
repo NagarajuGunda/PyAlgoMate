42b0eb01b7627034db7d01db5be456e3ceeb568f
Nagaraju Gunda <gunda.nagaraju92@gmail.com>
2024-03-03 11:19:04 +0530

    Added collected premium, current premium and mtm to trades view

--- flet/views/trades.py
+++ flet/views/trades.py
@@ -15,6 +15,65 @@ class TradesView(ft.View):
                 bgcolor=ft.colors.SURFACE_VARIANT
             )
 
+        self.collectedPremiumText = ft.Text("₹ 0", color=ft.colors.GREEN, size=25)
+        self.currentPremiumText = ft.Text("₹ 0", color=ft.colors.GREEN, size=25)
+        self.mtmText = ft.Text("₹ 0", color=ft.colors.GREEN, size=25)
+        self.premiumsCard = ft.Card(
+            ft.Container(
+                ft.Row(
+                    [
+                        ft.Column(
+                            [
+                                    ft.Container(
+                                        content=ft.Text("Collected Premium", color=ft.colors.BLACK38, size=15)
+                                    ),
+                                    ft.Container(
+                                        content=self.collectedPremiumText
+                                    ),
+                            ],
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                            expand=True,
+                        ),
+                        ft.VerticalDivider(color=ft.colors.BLACK38, thickness=2, opacity=0.5),
+                        ft.Column(
+                            [
+                                    ft.Container(
+                                        content=ft.Text("Current Premium", color=ft.colors.BLACK38, size=15)
+                                    ),
+                                    ft.Container(
+                                        content=self.currentPremiumText
+                                    ),
+                            ],
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                            expand=True,
+                        ),
+                        ft.VerticalDivider(color=ft.colors.BLACK38, thickness=2, opacity=0.5),
+                        ft.Column(
+                            [
+                                    ft.Container(
+                                        content=ft.Text("MTM", color=ft.colors.BLACK38, size=15)
+                                    ),
+                                    ft.Container(
+                                        content=self.mtmText
+                                    ),
+                            ],
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                            expand=True,
+                        ),
+                    ],
+                    spacing=0,
+                    expand=True,
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                ),
+                padding=ft.padding.all(20),
+                expand=True
+            ),
+            height=120
+        )
+
         columns=[
             ft.DataColumn(ft.Text("Instrument")),
             ft.DataColumn(ft.Text("B/S")),
@@ -36,9 +95,10 @@ class TradesView(ft.View):
 
         self.controls = [
             ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+            self.premiumsCard,
             self.datatable
         ]
-    
+
     def getRows(self):
         rows = []
         for pos in self.strategy.getActivePositions().copy().union(self.strategy.getClosedPositions().copy()):
@@ -67,7 +127,41 @@ class TradesView(ft.View):
                 )
             )
         return rows
-    
+
     def update(self):
         self.datatable.rows = self.getRows()
-        super().update()
+        self.datatable.update()
+
+        openPositions = self.strategy.getActivePositions().copy()
+        closedPositions = self.strategy.getClosedPositions().copy()
+
+        collectedPremium = sum(
+                [
+                    position.getEntryOrder().getAvgFillPrice() * position.getEntryOrder().getQuantity()
+                    for position in openPositions.union(closedPositions)
+                    if position.entryFilled() and position.getEntryOrder().isSell()
+                ]
+            )
+        self.collectedPremiumText.value = f'₹ {collectedPremium:.2f}'
+        self.collectedPremiumText.color = "green" if collectedPremium >= 0 else "red"
+        self.collectedPremiumText.update()
+
+        currentPremium = sum(
+            [
+                position.getLastPrice() * position.getEntryOrder().getQuantity()
+                for position in openPositions
+                if position.entryFilled() and position.getEntryOrder().isSell()
+            ]
+        )
+        self.currentPremiumText.value = f'₹ {currentPremium:.2f}'
+        self.currentPremiumText.color = "green" if currentPremium >= 0 else "red"
+        self.currentPremiumText.update()
+
+        mtm = sum(
+            [
+                position.getPnL() for position in openPositions.union(closedPositions)
+            ]
+        )
+        self.mtmText.value = f'₹ {mtm:.2f}'
+        self.mtmText.color = "green" if mtm >= 0 else "red"
+        self.mtmText.update()
