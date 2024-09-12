669f2a9ccdd34cb3feffc743b0e12eefd3b9f3a0
Nagaraju Gunda <gunda.nagaraju98@gmail.com>
2024-09-13 00:50:12 +0530

    Tests

--- tests/test_finvasia_trade_monitor.py
+++ tests/test_finvasia_trade_monitor.py
@@ -1,39 +1,34 @@
-import pytest
-from unittest.mock import Mock, patch, AsyncMock, MagicMock
-import asyncio
 import queue
+from unittest.mock import AsyncMock, Mock, patch
+
+import pytest
+
 from pyalgomate.brokers.finvasia.broker import (
-    TradeMonitor,
-    OrderEvent,
     LiveBroker,
     Order,
+    TradeMonitor,
 )
 
 
 @pytest.fixture
 def trade_monitor():
     mock_broker = Mock(spec=LiveBroker)
-    with patch(
-        "pyalgomate.brokers.finvasia.broker.OrderUpdateThread"
-    ) as mock_order_update_thread:
-        monitor = TradeMonitor(mock_broker)
-        monitor._TradeMonitor__zmq_update_thread = mock_order_update_thread.return_value
-        yield monitor
+    mock_broker._LiveBroker__orderUpdateThread = Mock()
+    mock_broker._LiveBroker__orderUpdateThread.getQueue.return_value = queue.Queue()
+    monitor = TradeMonitor(mock_broker)
+    yield monitor
 
 
 class TestTradeMonitor:
 
     def test_init(self, trade_monitor):
-        assert isinstance(trade_monitor._TradeMonitor__queue, queue.Queue)
-        assert not trade_monitor._TradeMonitor__stop
+        assert trade_monitor._TradeMonitor__stop == False
         assert trade_monitor._TradeMonitor__retryData == {}
         assert trade_monitor._TradeMonitor__pendingUpdates == set()
-        trade_monitor._TradeMonitor__zmq_update_thread.start.assert_called_once()
 
     @pytest.mark.asyncio
-    async def test_process_order_event_rejected(self, trade_monitor):
+    async def test_process_order_update_rejected(self, trade_monitor):
         mock_order_update = {
-            "t": "om",
             "norenordno": "24082700978090",
             "status": "REJECTED",
             "rejreason": "SAF:order is not open to cancel",
@@ -43,15 +38,14 @@ class TestTradeMonitor:
         mock_order.getRemarks.return_value = "PyAlgoMate order 129094422825040"
         trade_monitor._TradeMonitor__broker.getActiveOrders.return_value = [mock_order]
 
-        await trade_monitor.processOrderUpdate(mock_order, mock_order_update, [])
+        await trade_monitor.processOrderUpdate(mock_order_update)
 
         assert len(trade_monitor._TradeMonitor__retryData) == 1
         assert trade_monitor._TradeMonitor__retryData[mock_order]["retryCount"] == 1
 
     @pytest.mark.asyncio
-    async def test_process_order_event_complete(self, trade_monitor):
+    async def test_process_order_update_complete(self, trade_monitor):
         mock_order_update = {
-            "t": "om",
             "norenordno": "24082700978090",
             "status": "COMPLETE",
             "remarks": "PyAlgoMate order 129094422825040",
@@ -60,100 +54,16 @@ class TestTradeMonitor:
         mock_order.getRemarks.return_value = "PyAlgoMate order 129094422825040"
         trade_monitor._TradeMonitor__broker.getActiveOrders.return_value = [mock_order]
 
-        trades = []
-        await trade_monitor.processOrderUpdate(mock_order, mock_order_update, trades)
+        with patch.object(
+            trade_monitor, "processOrderEvent", new_callable=AsyncMock
+        ) as mock_process_event:
+            await trade_monitor.processOrderUpdate(mock_order_update)
 
-        assert len(trades) == 1
-        assert trades[0].getStatus() == "COMPLETE"
+        mock_process_event.assert_called_once()
 
     def test_stop(self, trade_monitor):
         trade_monitor.stop()
-
-        assert trade_monitor._TradeMonitor__stop
-        trade_monitor._TradeMonitor__zmq_update_thread.stop.assert_called_once()
-        trade_monitor._TradeMonitor__zmq_update_thread.join.assert_called_once()
-
-    @pytest.mark.asyncio
-    async def test_process_pending_updates(self, trade_monitor):
-        mock_order_update = {
-            "t": "om",
-            "norenordno": "24082700978090",
-            "status": "COMPLETE",
-            "remarks": "PyAlgoMate order 129094422825040",
-        }
-        trade_monitor._TradeMonitor__pendingUpdates = set(
-            [frozenset(mock_order_update.items())]
-        )
-        mock_order = Mock(spec=Order)
-        mock_order.getRemarks.return_value = "PyAlgoMate order 129094422825040"
-        trade_monitor._TradeMonitor__broker.getActiveOrders.return_value = [mock_order]
-
-        with patch.object(
-            trade_monitor, "processOrderUpdate", new_callable=AsyncMock
-        ) as mock_process:
-            # Mock the run_async method to process pending updates directly
-            async def mock_run_async():
-                while trade_monitor._TradeMonitor__pendingUpdates:
-                    update = trade_monitor._TradeMonitor__pendingUpdates.pop()
-                    update_dict = dict(update)
-                    await trade_monitor.processOrderUpdate(mock_order, update_dict, [])
-                trade_monitor._TradeMonitor__stop = True
-
-            with patch.object(trade_monitor, "run_async", side_effect=mock_run_async):
-                await trade_monitor.run_async()
-
-        mock_process.assert_called_once()
-        assert len(trade_monitor._TradeMonitor__pendingUpdates) == 0
-
-    @pytest.mark.asyncio
-    async def test_process_order_update_delayed_order(self, trade_monitor):
-        mock_order_update = {
-            "t": "om",
-            "norenordno": "24082700978090",
-            "status": "COMPLETE",
-            "remarks": "PyAlgoMate order 129094422825040",
-        }
-
-        # Simulate order not found initially
-        trade_monitor._TradeMonitor__broker.getActiveOrders.return_value = []
-
-        # Simulate the behavior of run_async when order is not found
-        trade_monitor._TradeMonitor__pendingUpdates.add(
-            frozenset(mock_order_update.items())
-        )
-
-        assert len(trade_monitor._TradeMonitor__pendingUpdates) == 1
-
-        # Now simulate the order being found
-        mock_order = Mock(spec=Order)
-        mock_order.getRemarks.return_value = "PyAlgoMate order 129094422825040"
-        trade_monitor._TradeMonitor__broker.getActiveOrders.return_value = [mock_order]
-
-        # Mock the processOrderUpdate method
-        with patch.object(
-            trade_monitor, "processOrderUpdate", new_callable=AsyncMock
-        ) as mock_process:
-            # Simulate the behavior of run_async when processing pending updates
-            async def mock_run_async():
-                for orderUpdate in list(trade_monitor._TradeMonitor__pendingUpdates):
-                    update_dict = dict(orderUpdate)
-                    order = next(
-                        (
-                            o
-                            for o in trade_monitor._TradeMonitor__broker.getActiveOrders()
-                            if o.getRemarks() == update_dict.get("remarks")
-                        ),
-                        None,
-                    )
-                    if order:
-                        await trade_monitor.processOrderUpdate(order, update_dict, [])
-                        trade_monitor._TradeMonitor__pendingUpdates.remove(orderUpdate)
-
-            await mock_run_async()
-
-        assert len(trade_monitor._TradeMonitor__pendingUpdates) == 0
-        trade_monitor._TradeMonitor__broker.getActiveOrders.assert_called()
-        mock_process.assert_called_once_with(mock_order, mock_order_update, [])
+        assert trade_monitor._TradeMonitor__stop == True
 
 
 if __name__ == "__main__":
