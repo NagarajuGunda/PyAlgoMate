ed95ce957305c43f1002a0515d7b385b4d204bcc
ArunRaj <k2arunraj@gmail.com>
2024-12-24 14:47:33 +0530

    Add flattrade broker

---
+++ pyalgomate/brokers/flattrade/__init__.py
@@ -0,0 +1,306 @@
+"""
+.. moduleauthor:: Nagaraju Gunda
+"""
+
+import datetime
+import logging
+import os
+import re
+from concurrent.futures import ThreadPoolExecutor
+from functools import lru_cache
+from io import BytesIO, StringIO
+from zipfile import ZipFile
+from NorenRestApi_Flattrade.NorenApi_Flattrade import NorenApi_Flattrade as FlattradeApi
+import time
+
+import pandas as pd
+import pyotp
+import requests
+
+
+import pyalgomate.utils as utils
+from pyalgomate.brokers import getDefaultUnderlyings, getExpiryDates
+from pyalgomate.utils import UnderlyingIndex
+
+logger = logging.getLogger()
+
+urls = [
+    "https://flattrade.s3.ap-south-1.amazonaws.com/scripmaster/NSE_Equity.csv",
+    "https://flattrade.s3.ap-south-1.amazonaws.com/scripmaster/Nfo_Index_Derivatives.csv",
+    "https://flattrade.s3.ap-south-1.amazonaws.com/scripmaster/Commodity.csv",
+    "https://flattrade.s3.ap-south-1.amazonaws.com/scripmaster/BSE_Equity.csv",
+    "https://flattrade.s3.ap-south-1.amazonaws.com/scripmaster/Bfo_Equity_Derivatives.csv"
+]
+
+underlyingMapping = {
+    'NSE|MIDCPNIFTY': {
+        'optionPrefix': 'NFO|MIDCPNIFTY',
+        'index': UnderlyingIndex.MIDCPNIFTY,
+        'lotSize': 50,
+        'strikeDifference': 25
+    },
+    'NSE|NIFTY BANK': {
+        'optionPrefix': 'NFO|BANKNIFTY',
+        'index': UnderlyingIndex.BANKNIFTY,
+        'lotSize': 15,
+        'strikeDifference': 100
+    },
+    'NSE|NIFTY INDEX': {
+        'optionPrefix': 'NFO|NIFTY',
+        'index': UnderlyingIndex.NIFTY,
+        'lotSize': 25,
+        'strikeDifference': 50
+    },
+    'NSE|Nifty 50': {
+        'optionPrefix': 'NFO|NIFTY',
+        'index': UnderlyingIndex.NIFTY,
+        'lotSize': 25,
+        'strikeDifference': 50
+    },
+    'NSE|FINNIFTY': {
+        'optionPrefix': 'NFO|FINNIFTY',
+        'index': UnderlyingIndex.FINNIFTY,
+        'lotSize': 25,
+        'strikeDifference': 50
+    },
+    'BSE|SENSEX': {
+        'optionPrefix': 'BFO|SENSEX',
+        'index': UnderlyingIndex.SENSEX,
+        'lotSize': 10,
+        'strikeDifference': 100
+    },
+    'BSE|BANKEX': {
+        'optionPrefix': 'BFO|BANKEX',
+        'index': UnderlyingIndex.BANKEX,
+        'lotSize': 15,
+        'strikeDifference': 100
+    }
+}
+
+
+def downloadAndExtract(url):
+    logger.info(f'Downloading and extracting {url}')
+    response = requests.get(url)
+    content = response.content.decode("utf-8")
+    df = pd.read_csv(StringIO(content), delimiter=",")
+    return df
+
+
+@lru_cache(maxsize=None)
+def getScriptMaster(scripMasterFile='flattrade_symbols.csv') -> pd.DataFrame:
+    if os.path.exists(scripMasterFile) and \
+            (datetime.datetime.fromtimestamp(os.path.getmtime(scripMasterFile)).date() == datetime.datetime.today().date()):
+        logger.info(
+            "Scrip master that has been created today already exists. Using the same!"
+        )
+        return pd.read_csv(scripMasterFile)
+    else:
+        logger.info(
+            "Scrip master either doesn't exist or is not of today. Downloading!"
+        )
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(downloadAndExtract, url)
+                       for url in urls]
+
+            dfs = []
+            for future in futures:
+                df = future.result()
+                dfs.append(df)
+
+            scripMasterDf = pd.concat(dfs, ignore_index=True)
+            scripMasterDf.to_csv(scripMasterFile, index=False)
+            return scripMasterDf
+
+
+def getToken(instrument) -> str:
+    tokenMappings = getTokenMappings()
+    return tokenMappings[instrument]
+
+
+def getTokenMappings() -> dict:
+    scripMasterDf: pd.DataFrame = getScriptMaster()
+    tokenMappings = dict(zip(scripMasterDf['Exchange'] + '|' + scripMasterDf['Tradingsymbol'],
+                         scripMasterDf['Exchange'] + '|' + scripMasterDf['Token'].astype(str)))
+    tokenMappings['BSE|SENSEX'] = 'BSE|1'
+    tokenMappings['BSE|BANKEX'] = 'BSE|12'
+    return tokenMappings
+
+
+def getApi(cred):
+    api = FlattradeApi(host='https://piconnect.flattrade.in/PiConnectTP/',
+                       websocket='wss://piconnect.flattrade.in/PiConnectWSTp/')
+    userToken = None
+    tokenFile = 'flattradekey.txt'
+    if os.path.exists(tokenFile) and (datetime.datetime.fromtimestamp(os.path.getmtime(tokenFile)).date() == datetime.datetime.today().date()):
+        logger.info("Token has been created today already. Re-using it")
+        with open(tokenFile, 'r') as f:
+            userToken = f.read()
+        logger.info(
+            f"userid {cred['user']} password ******** usertoken {userToken}")
+        loginStatus = api.set_session(
+            userid=cred['user'], password=cred['pwd'], usertoken=userToken)
+    else:
+        logger.info("Logging in and persisting user token")
+        userToken = api.login(
+            userid=cred["user"],
+            password=cred["pwd"],
+            twoFA=pyotp.TOTP(cred["factor2"]).now(),
+            api_key=cred["apikey"],
+            api_secret=cred["apisecret"],
+        )
+
+        if userToken:
+            with open(tokenFile, "w") as f:
+                f.write(userToken)
+        else:
+            raise ValueError("Login failed")
+        logger.info("Logging in and persisting user token")
+
+        loginStatus = api.set_session(
+            userid=cred['user'], password=cred['pwd'], usertoken=userToken)
+
+    if loginStatus != None:
+        return api
+    else:
+        return None
+
+
+def getApiAndTokenMappings(cred, registerOptions, underlyings):
+    from .broker import getOptionSymbols, getUnderlyingDetails  # Lazy import
+
+    api = getApi(cred)
+    if api != None:
+        if len(underlyings) == 0:
+            underlyings = [underlying.replace(
+                ":", "|") for underlying in getDefaultUnderlyings()]
+
+        optionSymbols = []
+        futureSymbols = []
+        tokenMappings = getTokenMappings()
+        for underlying in underlyings:
+            if '-EQ' in underlying:
+                continue
+            exchange = underlying.split('|')[0]
+            underlyingToken = tokenMappings[underlying].split('|')[1]
+            logger.info(
+                f'Token id for <{underlying}> is <{underlyingToken}>')
+            if underlyingToken is None:
+                logger.error(
+                    f'Error getting token id for {underlyingToken}')
+                exit(1)
+            underlyingQuotes = api.get_quotes(exchange, underlyingToken)
+            ltp = underlyingQuotes['lp']
+
+            try:
+                underlyingDetails = getUnderlyingDetails(
+                    underlying)
+                index = underlyingDetails['index']
+                strikeDifference = underlyingDetails['strikeDifference']
+
+                (currentWeeklyExpiry, nextWeekExpiry,
+                 monthlyExpiry) = getExpiryDates(index)
+                futureSymbol = getFutureSymbol(index, monthlyExpiry)
+                futureSymbols.append(futureSymbol)
+                if "Weekly" in registerOptions:
+                    optionSymbols += getOptionSymbols(
+                        underlying, currentWeeklyExpiry, ltp, 20, strikeDifference)
+                if "NextWeekly" in registerOptions:
+                    optionSymbols += getOptionSymbols(
+                        underlying, nextWeekExpiry, ltp, 20, strikeDifference)
+                if "Monthly" in registerOptions:
+                    optionSymbols += getOptionSymbols(
+                        underlying, monthlyExpiry, ltp, 20, strikeDifference)
+            except Exception as e:
+                logger.exception(f'Exception: {e}')
+
+
+
+        optionSymbols = list(dict.fromkeys(optionSymbols))
+
+        instruments = underlyings + futureSymbols + optionSymbols
+
+        filteredTokenMappings = {
+            tokenMappings[instrument]: instrument for instrument in instruments if instrument in tokenMappings}
+
+        return api, filteredTokenMappings
+    else:
+        exit(1)
+
+
+def getFeed(cred, registerOptions, underlyings):
+    from .feed import LiveTradeFeed
+
+    logger.info('Creating feed object')
+    api, tokenMappings = getApiAndTokenMappings(
+        cred, registerOptions, underlyings)
+    return LiveTradeFeed(api, getTokenMappings(), tokenMappings.values()), api
+
+@lru_cache
+def getOptionContract(symbol):
+    from .broker import OptionContract
+
+    m = re.match(r"([A-Z\|]+)(\d{2})([A-Z]{3})(\d{2})([CP])(\d+)", symbol)
+
+    if m is None:
+        m = re.match(r"([A-Z\|]+)(\d{2})([A-Z]{3})(\d+)([CP])E", symbol)
+
+        if m is not None:
+            optionPrefix = m.group(1)
+            for underlying, underlyingDetails in underlyingMapping.items():
+                if underlyingDetails['optionPrefix'] == optionPrefix:
+                    index = underlyingDetails['index']
+                    month = datetime.datetime.strptime(m.group(3), '%b').month
+                    year = int(m.group(2)) + 2000
+                    expiry = utils.getNearestMonthlyExpiryDate(
+                        datetime.date(year, month, 1), index)
+                    return OptionContract(symbol, int(m.group(4)), expiry, "c" if m.group(5) == "C" else "p", underlying)
+
+        m = re.match(r"([A-Z\|]+)(\d{2})(\d|[OND])(\d{2})(\d+)([CP])E", symbol)
+
+        if m is None:
+            return None
+
+        day = int(m.group(4))
+        month = m.group(3)
+        if month == 'O':
+            month = 10
+        elif month == 'N':
+            month = 11
+        elif month == 'D':
+            month = 12
+        else:
+            month = int(month)
+
+        year = int(m.group(2)) + 2000
+        expiry = datetime.date(year, month, day)
+        optionPrefix = m.group(1)
+        for underlying, underlyingDetails in underlyingMapping.items():
+            if underlyingDetails['optionPrefix'] == optionPrefix:
+                return OptionContract(symbol, int(m.group(5)), expiry, "c" if m.group(6) == "C" else "p", underlying)
+
+    day = int(m.group(2))
+    month = m.group(3)
+    year = int(m.group(4)) + 2000
+    expiry = datetime.date(
+        year, datetime.datetime.strptime(month, '%b').month, day)
+
+    optionPrefix = m.group(1)
+
+    for underlying, underlyingDetails in underlyingMapping.items():
+        if underlyingDetails['optionPrefix'] == optionPrefix:
+            return OptionContract(symbol, int(m.group(6)), expiry, "c" if m.group(5) == "C" else "p", underlying)
+
+
+def getFutureSymbol(underlyingIndex: UnderlyingIndex, expiry: datetime.date):
+    scripMasterDf: pd.DataFrame = getScriptMaster()
+    scripMasterDf["Expiry"] = pd.to_datetime(scripMasterDf["Expiry"], format="%d-%b-%Y")
+    futureRow = scripMasterDf[
+        (scripMasterDf["Expiry"].dt.date == expiry)
+        & (scripMasterDf["Instrument"] == "FUTIDX")
+        & scripMasterDf["Tradingsymbol"].str.startswith(str(underlyingIndex))
+    ].iloc[0]
+    return futureRow["Exchange"] + "|" + futureRow["Tradingsymbol"]
+
+
+if __name__ == '__main__':
+    print(getScriptMaster())
---
+++ pyalgomate/brokers/flattrade/feed.py
@@ -0,0 +1,360 @@
+"""
+.. moduleauthor:: Nagaraju Gunda
+"""
+
+import asyncio
+import datetime
+import logging
+import os
+import pickle
+import tempfile
+import threading
+import traceback
+import numpy as np
+from queue import Queue
+from typing import Optional, Tuple
+
+import zmq
+import zmq.asyncio
+from NorenRestApi_Flattrade.NorenApi_Flattrade import NorenApi_Flattrade
+from pyalgotrade import bar
+
+from pyalgomate.barfeed import BaseBarFeed
+from pyalgomate.barfeed.BasicBarEx import BasicBarEx
+from pyalgomate.core import OptionType
+
+from . import getOptionContract
+
+logger = logging.getLogger(__name__)
+
+
+class QuoteMessage(object):
+    # t	tk	‘tk’ represents touchline acknowledgement
+    # e	NSE, BSE, NFO ..	Exchange name
+    # tk	22	Scrip Token
+    # pp	2 for NSE, BSE & 4 for CDS USDINR	Price precision
+    # ts		Trading Symbol
+    # ti		Tick size
+    # ls		Lot size
+    # lp		LTP
+    # pc		Percentage change
+    # v		volume
+    # o		Open price
+    # h		High price
+    # l		Low price
+    # c		Close price
+    # ap		Average trade price
+    # oi		Open interest
+    # poi		Previous day closing Open Interest
+    # toi		Total open interest for underlying
+    # bq1		Best Buy Quantity 1
+    # bp1		Best Buy Price 1
+    # sq1		Best Sell Quantity 1
+    # sp1		Best Sell Price 1
+
+    def __init__(self, eventDict, tokenMappings):
+        self.__eventDict = eventDict
+        self.__tokenMappings = tokenMappings
+
+    def __str__(self):
+        return f"{self.__eventDict}"
+
+    @property
+    def field(self):
+        return self.__eventDict["t"]
+
+    @property
+    def exchange(self):
+        return self.__eventDict["e"]
+
+    @property
+    def scriptToken(self):
+        return self.__eventDict["tk"]
+
+    @property
+    def dateTime(self):
+        return self.__eventDict["ft"]
+
+    @property
+    def price(self):
+        return float(self.__eventDict.get("lp", 0))
+
+    @property
+    def microprice(self):
+        try:
+            bbp = float(self.__eventDict.get("bp1", 0))  # Buying Price 1
+            bap = float(self.__eventDict.get("sp1", 0))  # Sell Price 1
+            bbq = int(self.__eventDict.get("bq1", 0))  # Buying quantity 1
+            baq = int(self.__eventDict.get("sq1", 0))  # Sell Qty 1
+            mpp = ((bap * bbq) + (bbp * baq)) / (bbq + baq)
+            p = int(1 / 0.05)  # 0.05 is tick size
+            return float(np.ceil(mpp * p) / p)
+        except Exception:
+            return self.price
+
+    @property
+    def volume(self):
+        return float(self.__eventDict.get("volume", self.__eventDict.get("v", 0)))
+
+    @property
+    def openInterest(self):
+        return float(self.__eventDict.get("oi", 0))
+
+    @property
+    def seq(self):
+        return int(self.dateTime)
+
+    @property
+    def instrument(self):
+        return f"{self.exchange}|{self.__tokenMappings[f'{self.exchange}|{self.scriptToken}'].split('|')[1]}"
+
+    def getBar(self, dateTime=None) -> BasicBarEx:
+        open = high = low = close = self.price
+
+        return BasicBarEx(
+            dateTime or self.dateTime,
+            open,
+            high,
+            low,
+            close,
+            self.volume,
+            None,
+            bar.Frequency.TRADE,
+            {
+                "Instrument": self.instrument,
+                "Open Interest": self.openInterest,
+                "Message": self.__eventDict,
+            },
+        )
+
+
+class LiveTradeFeed(BaseBarFeed):
+
+    def __init__(
+        self,
+        api: NorenApi_Flattrade,
+        tokenMappings: dict,
+        instruments: list,
+        ipc_path=None,
+        timeout=10,
+        maxLen=None,
+    ):
+        super(LiveTradeFeed, self).__init__(bar.Frequency.TRADE, maxLen)
+        self.__instruments = instruments
+        self.__instrumentToTokenIdMapping = {
+            instrument: tokenMappings[instrument]
+            for instrument in self.__instruments
+            if instrument in tokenMappings
+        }
+        self.__tokenIdToInstrumentMappings = {
+            value: key for key, value in tokenMappings.items()
+        }
+
+        if len(self.__instruments) != len(self.__instrumentToTokenIdMapping):
+            raise Exception(
+                f"Could not get tokens for the instruments {[instrument for instrument in self.__instruments if instrument not in tokenMappings]}"
+            )
+
+        self.__api = api
+
+        for key, value in self.__instrumentToTokenIdMapping.items():
+            self.registerDataSeries(key)
+
+        self.__stopped = False
+        self.__lastQuoteDateTime = None
+        self.__lastReceivedDateTime = None
+        self.__lastUpdateTime = None
+        self.__nextBarsTime = None
+
+        # ZeroMQ setup
+        self.__context = zmq.asyncio.Context()
+        self.__socket = self.__context.socket(zmq.SUB)
+
+        if ipc_path is None:
+            # Create a platform-independent IPC path
+            ipc_dir = tempfile.gettempdir()
+            ipc_file = "pyalgomate_ipc"
+            self.__ipc_path = os.path.join(ipc_dir, ipc_file)
+        else:
+            self.__ipc_path = ipc_path
+
+        # On Windows, we need to use tcp instead of ipc
+        if os.name == "nt":
+            self.__socket.connect(f"tcp://127.0.0.1:{self.__ipc_path.split(':')[-1]}")
+        else:
+            self.__socket.connect(f"ipc://{self.__ipc_path}")
+
+        self.__socket.setsockopt_string(zmq.SUBSCRIBE, "FEED_UPDATE")
+
+        self.__latestQuotes = {}
+        self.__latestOIs = {}
+
+        # Thread to run the asyncio event loop
+        self.__loopThread = threading.Thread(target=self.__run_event_loop)
+        self.__loopThread.start()
+
+    def getApi(self):
+        return self.__api
+
+    def getCurrentDateTime(self):
+        return datetime.datetime.now()
+
+    def barsHaveAdjClose(self):
+        return False
+
+    def getLastBar(self, instrument):
+        lastBarQuote = self.__latestQuotes.get(
+            self.__instrumentToTokenIdMapping[instrument], None
+        )
+        if lastBarQuote is not None:
+            return QuoteMessage(
+                lastBarQuote, self.__tokenIdToInstrumentMappings
+            ).getBar()
+        return None
+
+    def getMicroPrice(self, instrument) -> float:
+        lastBarQuote = self.__latestQuotes.get(
+            self.__instrumentToTokenIdMapping[instrument], None
+        )
+        if lastBarQuote is not None:
+            return float(
+                QuoteMessage(
+                    lastBarQuote, self.__tokenIdToInstrumentMappings
+                ).microprice
+            )
+        return 0
+
+    def __run_event_loop(self):
+        asyncio.run(self.__async_main())
+
+    async def __async_main(self):
+        while not self.__stopped:
+            try:
+                topic, message = await self.__socket.recv_multipart(flags=zmq.NOBLOCK)
+                message = pickle.loads(message)
+                key = message["e"] + "|" + message["tk"]
+                if float(message.get("lp", 0)) <= 0:
+                    continue
+                if key in self.__latestQuotes:
+                    symbolInfo = self.__latestQuotes[key]
+                    symbolInfo.update(message)
+                    self.__latestQuotes[key] = symbolInfo
+                else:
+                    self.__latestQuotes[key] = message
+
+                if message.get("oi", None) is not None:
+                    self.__latestOIs[key] = float(message["oi"])
+
+                self.__lastQuoteDateTime = message["ft"]
+                self.__lastReceivedDateTime = datetime.datetime.now()
+            except zmq.Again:
+                await asyncio.sleep(0.01)
+
+    def getNextBars(self):
+        def getBar(message, lastQuoteDateTime):
+            if not message.get("oi", None):
+                message["oi"] = self.__latestOIs.get(
+                    message["e"] + "|" + message["tk"], 0
+                )
+            bar = QuoteMessage(message, self.__tokenIdToInstrumentMappings).getBar(
+                lastQuoteDateTime
+            )
+            return bar.getInstrument(), bar
+
+        bars = None
+        lastQuoteDateTime = self.__lastQuoteDateTime
+        if self.__lastUpdateTime != lastQuoteDateTime:
+            self.__nextBarsTime = datetime.datetime.now()
+            self.__lastUpdateTime = lastQuoteDateTime
+            barDateTime = self.__nextBarsTime.replace(microsecond=0)
+            bars = bar.Bars(
+                {
+                    instrument: bar
+                    for quoteMessage in list(self.__latestQuotes.values())
+                    for instrument, bar in [getBar(quoteMessage, barDateTime)]
+                }
+            )
+        return bars
+
+    def peekDateTime(self):
+        return None
+
+    def start(self):
+        super(LiveTradeFeed, self).start()
+        logger.info("LiveTradeFeed started")
+
+    def dispatch(self):
+        try:
+            ret = False
+            if super(LiveTradeFeed, self).dispatch():
+                ret = True
+            return ret
+        except Exception as e:
+            logger.error(f"Exception: {e}")
+            logger.exception(traceback.format_exc())
+
+    def stop(self):
+        self.__stopped = True
+        self.__socket.close()
+        self.__context.term()
+        self.__loopThread.join()
+
+    def join(self):
+        pass
+
+    def eof(self):
+        return self.__stopped
+
+    def getOrderBookUpdateEvent(self):
+        return None
+
+    def getLastUpdatedDateTime(self):
+        return self.__lastQuoteDateTime
+
+    def getLastReceivedDateTime(self):
+        return self.__lastReceivedDateTime
+
+    def getNextBarsDateTime(self):
+        return self.__nextBarsTime
+
+    def isDataFeedAlive(self, heartBeatInterval=5):
+        if self.__lastQuoteDateTime is None:
+            return False
+
+        currentDateTime = datetime.datetime.now()
+        timeSinceLastDateTime = currentDateTime - self.__lastQuoteDateTime
+        return timeSinceLastDateTime.total_seconds() <= heartBeatInterval
+
+    def findNearestPremiumOption(
+        self,
+        expiry: datetime.datetime,
+        optionType: OptionType,
+        premium: float,
+        time: datetime.datetime,
+    ) -> Optional[Tuple[str, float]]:
+        nearestOption = None
+        nearestPremium = None
+        minDifference = float("inf")
+
+        for tokenId, quote in self.__latestQuotes.items():
+            instrument = self.__tokenIdToInstrumentMappings[tokenId]
+            optionContract = getOptionContract(instrument)
+
+            if (
+                optionContract is None
+                or optionContract.expiry != expiry
+                or optionContract.type
+                != ("c" if optionType == OptionType.CALL else "p")
+            ):
+                continue
+
+            close = QuoteMessage(quote, self.__tokenIdToInstrumentMappings).price
+
+            difference = abs(close - premium)
+
+            if difference < minDifference:
+                minDifference = difference
+                nearestOption = instrument
+                nearestPremium = close
+
+        return nearestOption, nearestPremium
